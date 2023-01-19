2c244c5fa53a4fb75fe130280896b212016ee121
Berezin Konstantin <berezinko90@gmail.com>
2023-01-19 08:46:12 +0700

    change: styles

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,9 +4,11 @@ import db from "../../data_bases/cat_foods.json";
 
 function App() {
   return (
-    <div className="app">
-      <div className="app__title">Ты сегодня покормил кота?</div>
-      <CardList foodlist={db.food} />
+    <div className="app__overlay">
+      <div className="app">
+        <div className="app__title">Ты сегодня покормил кота?</div>
+        <CardList foodlist={db.food} />
+      </div>
     </div>
   );
 }
